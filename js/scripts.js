fb9caf80952a776a13dc1f0df7cb9ff588dbbc89
Derrick Trang <derricktrang@gmail.com>
2017-05-19 23:20:09 -1000

    Navbar practice

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 console.log("scripts.js executing...");
 
-function loadFunction() {
-    console.log("load function called");
+function loadNavbar() {
+    console.log("Load navbar function call start");
     var link = document.querySelector('link[rel="import"]');
     var content = link.import;
 
     var el = content.querySelector('.navbar');
     document.getElementById("nav-placeholder").appendChild(el.cloneNode(true));
+    console.log("Load navbar function call end");
 }
