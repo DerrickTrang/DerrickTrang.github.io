19153903fc996aebb76942dd659194e49d7fe2cb
Derrick Trang <derricktrang@gmail.com>
2017-05-19 22:25:17 -1000

    Navbar practice
    Trying to import navbar.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 
+
+
 var x = "test";
 var y = "clicked again";
 var helloText = document.getElementById("hello-tag");
@@ -18,5 +20,9 @@ function functio() {
 }
 
 function loadFunction() {
+    var link = document.querySelector('link[rel="import"]');
+    var content = link.import;
 
+    var el = content.querySelector('.navbar');
+    document.getElementById("nav-placeholder").appendChild(el.cloneNode(true));
 }
