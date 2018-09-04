c7f9f978608364dcdadea7a07715499e9ded6ad8
Derrick Trang <derricktrang@gmail.com>
2018-09-03 17:39:26 -1000

    Revert "Navbar practice"
    This reverts commit fb9caf80952a776a13dc1f0df7cb9ff588dbbc89.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
 console.log("scripts.js executing...");
 
-function loadNavbar() {
-    console.log("Load navbar function call start");
+function loadFunction() {
+    console.log("load function called");
     var link = document.querySelector('link[rel="import"]');
     var content = link.import;
 
     var el = content.querySelector('.navbar');
     document.getElementById("nav-placeholder").appendChild(el.cloneNode(true));
-    console.log("Load navbar function call end");
 }
