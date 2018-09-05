706d85bf833a154a9becf0028cbf226a83e76989
Derrick Trang <derricktrang@gmail.com>
2018-09-04 23:31:47 -1000

    Hourglass v1.5
    Added color, a/b states

--- js/hourglass.js
+++ js/hourglass.js
@@ -4,13 +4,16 @@ var countdownStart = 60000; // countdown from 60000 ms (let user edit this later
 var countdownElapsed = 0; // total amount of time that has elapsed\
 var countdownDelta = 0;
 var startDate;
+var currentState;
+var lastState;
 
-var startButton = document.getElementById('startButton');
-var pauseButton = document.getElementById('pauseButton');
-var resumeButton = document.getElementById('resumeButton');
-var resetButton = document.getElementById('resetButton');
-var flipButton = document.getElementById('flipButton');
-var hourglass = document.getElementById('hourglass');
+var startButtonA;
+var startButtonB;
+var pauseButton;
+var resumeButton;
+var resetButton;
+var flipButton;
+var hourglass;
 
 function showTimerValue() {
     
@@ -19,13 +22,13 @@ function showTimerValue() {
         console.log("Expired!");
         hourglass.innerHTML = formatTime(0);
         clearInterval(objInterval);
-        changeState(3);
+        changeState(4);
     } else {
         hourglass.innerHTML = formatTime(countdownStart - countdownElapsed - countdownDelta);
     }
 }
 
-function startTimer() {
+function startTimerA() {
     countdownElapsed = 0;
     countdownDelta = 0;
 
@@ -35,9 +38,22 @@ function startTimer() {
     // Set timer to refresh every 25 ms
     objInterval = setInterval(showTimerValue, 25)
 
-    
     // change state
     changeState(1);
+}
+
+function startTimerB() {
+    countdownElapsed = 0;
+    countdownDelta = 0;
+
+    // Initialize start time
+    startDate = Date.now();
+
+    // Set timer to refresh every 25 ms
+    objInterval = setInterval(showTimerValue, 25)
+
+    // change state
+    changeState(2);    
 }
 
 function pauseTimer() {
@@ -45,14 +61,15 @@ function pauseTimer() {
     clearInterval(objInterval);
     countdownElapsed += countdownDelta;
 
-    changeState(2);
+    lastState = currentState;
+    changeState(3);
 }
 
 function resumeTimer() {
     startDate = Date.now();
     objInterval = setInterval(showTimerValue, 25)
 
-    changeState(1);
+    changeState(lastState);
 }
 
 function resetTimer() {
@@ -68,13 +85,35 @@ function flipTimer() {
     // Get current time difference
     countdownElapsed = countdownStart - (countdownElapsed + countdownDelta);
     startDate = Date.now();
-    changeState(1);
+    if (currentState === 1) {
+        changeState(2);
+    } else {
+        changeState(1);
+    }    
 }
 
 function init() {
     countdownStart = 60000; // countdown from 60000 ms (let user edit this later)
     countdownElapsed = 0; // total amount of time that has elapsed\
     countdownDelta = 0;
+
+    // Retrieve all document elements
+    startButtonA = document.getElementById('startButtonA');
+    startButtonB = document.getElementById('startButtonB');
+    pauseButton = document.getElementById('pauseButton');
+    resumeButton = document.getElementById('resumeButton');
+    resetButton = document.getElementById('resetButton');
+    flipButton = document.getElementById('flipButton');
+    hourglassContainer = document.getElementById('hourglassContainer');
+    hourglass = document.getElementById('hourglass');
+
+    // Set event listeners
+    startButtonA.addEventListener("click", startTimerA);
+    startButtonB.addEventListener("click", startTimerB);
+    pauseButton.addEventListener("click", pauseTimer);
+    resumeButton.addEventListener("click", resumeTimer);
+    resetButton.addEventListener("click", resetTimer);
+    flipButton.addEventListener("click", flipTimer);
 
     hourglass.innerHTML = formatTime(countdownStart);
     
@@ -83,59 +122,86 @@ function init() {
 
 function changeState(state) {
     // Use this function to update various parameters and functions
+    currentState = state;
+    console.log("changing state to: " + state);
     switch (state) {
         case 0:
             // Start
             console.log("start state");
-            startButton.style.display = 'block';
+            startButtonA.style.display = 'block';
+            startButtonB.style.display = 'block';
             pauseButton.style.display = 'none';
             resumeButton.style.display = 'none';
             resetButton.style.display = 'none';
             flipButton.style.display = 'none';
 
+            hourglassContainer.style.backgroundColor = "white";
             hourglass.style.backgroundColor = "white";
             break;
         case 1:
-            // In progress
+            // In progress A
             console.log("in progress state");
-            startButton.style.display = 'none';
+            startButtonA.style.display = 'none';
+            startButtonB.style.display = 'none';
             pauseButton.style.display = 'block';
             resumeButton.style.display = 'none';
             resetButton.style.display = 'none';
             flipButton.style.display = 'block';
 
-            hourglass.style.backgroundColor = "white";
+            flipButton.style.backgroundColor = "dodgerblue"
+
+            hourglassContainer.style.backgroundColor = "coral";
+            hourglass.style.backgroundColor = "orangered";
             break;
         case 2:
+            // In progress B
+            console.log("in progress state");
+            startButtonA.style.display = 'none';
+            startButtonB.style.display = 'none';
+            pauseButton.style.display = 'block';
+            resumeButton.style.display = 'none';
+            resetButton.style.display = 'none';
+            flipButton.style.display = 'block';
+
+            flipButton.style.backgroundColor = "orangered"
+
+            hourglassContainer.style.backgroundColor = "skyblue";
+            hourglass.style.backgroundColor = "dodgerblue";
+            break;
+        case 3:
             // Stopped
             console.log("stopped state");
-            startButton.style.display = 'none';
+            startButtonA.style.display = 'none';
+            startButtonB.style.display = 'none';
             pauseButton.style.display = 'none';
             resumeButton.style.display = 'block';
             resetButton.style.display = 'block';
             flipButton.style.display = 'none';
 
-            hourglass.style.backgroundColor = "white";
             break;
-        case 3:
+        case 4:
             // Timer expired
             console.log("expired state");
-            startButton.style.display = 'none';
+            startButtonA.style.display = 'none';
+            startButtonB.style.display = 'none';
             pauseButton.style.display = 'none';
             resumeButton.style.display = 'none';
             resetButton.style.display = 'block';
             flipButton.style.display = 'none';
 
+            hourglassContainer.style.backgroundColor = "black";
             hourglass.style.backgroundColor = "red";
             break;
         default:
             // default is start
-            startButton.style.display = 'block';
+            startButtonA.style.display = 'block';
+            startButtonB.style.display = 'block';
             pauseButton.style.display = 'none';
             resumeButton.style.display = 'none';
             resetButton.style.display = 'none';
             flipButton.style.display = 'none';
 
+            hourglassContainer.style.backgroundColor = "white";
             hourglass.style.backgroundColor = "white";
             break;
     }
