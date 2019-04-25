365d1164e1414320569cfa27d62684683afa8c4c
Derrick Trang <derricktrang@gmail.com>
2019-04-24 23:04:43 -1000

    Updated look
    - removed link import, onload, loadnavbar function
    - spruced up the look

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,1 @@
 console.log("scripts.js executing...");
-
-function loadNavbar() {
-    console.log("Load navbar function call start");
-    var link = document.querySelector('link[rel="import"]');
-    var content = link.import;
-
-    var el = content.querySelector('.navbar');
-    document.getElementById("nav-placeholder").appendChild(el.cloneNode(true));
-    console.log("Load navbar function call end");
-}
